d43f12105db6f14e120bc27f0cca8845c645b224
Joe Dursun <joedursun@gmail.com>
2014-02-27 09:15:35 -0500

    Move selectors to separate module

---
+++ lib/selectors.rb
@@ -0,0 +1,17 @@
+module Selectors
+  def last_sale_selector
+    '#qwidget_lastsale'
+  end
+
+  def percent_diff_selector
+    '#qwidget_percent'
+  end
+
+  def points_diff_selector
+    '#qwidget_netchange'
+  end
+
+  def arrow_selector
+    '#qwidget-arrow-image'
+  end
+end
